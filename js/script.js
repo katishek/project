f466b56aeb223724f75f49e975240ab68da9a80e
Ekaterina <katishek77@gmail.com>
2020-05-10 18:33:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+var letBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(letBorderWidth);
+
+confirm("Are you here?");
+
+let answer = +prompt('Есть ли Вам 18?', 'Да');
+
+console.log(typeof(answer));
